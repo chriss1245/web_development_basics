033bef23fd3fe3993bde461a78919d7678e58a94
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2021-10-21 12:26:23 +0200

    modified the controler file so that it is compatible with the new model schema

--- lab03/main.py
+++ lab03/main.py
@@ -12,50 +12,76 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    user = model.User(1, "mary@example.com", "mary")
+    user = model.User(email= "mary@example.com", name= "mary")
     posts = [
         model.Message(
-            1, user, "Test post", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user,
+            text = "Test post", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ),
         model.Message(
-            2, user, "Another post", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user,
+            text = "Why i think we live in a parties dictatorship: \
+                We do not select governors, we select parties who decide \
+                who the govenors are.", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ),
         model.Message(
-            3, user, "Custom post", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
+            user = user,
+            text = "What do you guys think", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
+        )
     ]
     return render_template("main/index.html", posts=posts)
 
 
 @bp.route("/profile")
 def user():
-    user = model.User(1, "mary@example.com", "mary")
+    user = model.User(email= "mary@example.com", name= "mary")
     user.photo =  default.PHOTO
     posts = [
         model.Message(
-            1, user, "Test post", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user,
+            text = "Test post", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ),
         model.Message(
-            2, user, "Another post", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user,
+            text = "Why i think we live in a parties dictatorship: \
+                We do not select governors, we select parties who decide \
+                who the govenors are.", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ),
         model.Message(
-            3, user, "Custom post", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-    ]
+            user = user,
+            text = "What do you guys think", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
+        )
+    ]  
     return render_template("main/profile.html", user=user, posts=posts)
 
 @bp.route("/messages")
 def messages():
-    user = model.User(1, "mary@example.com", "Marie")
-    users = [model.User(i+2, name + '@example.com', name) for i, name in zip(range(4), ['emilio', 'pablo', 'rosa', 'hilda'])]
+    user = model.User(email= "mary@example.com", name= "mary")
+    users = [model.User(email= name+ "@example.com", name= name)\
+         for name in ['emilio', 'pablo', 'rosa', 'hilda']]
 
 
     message = model.Message(
-            1, user, "I am testing if this awsome page works", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user,
+            text = "Why i think we live in a parties dictatorship: \
+                We do not select governors, we select parties who decide \
+                who the govenors are.", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         )
 
-    replies = [model.Message(i+2, 
-                users[i],
-                text,
-                datetime.datetime.now(dateutil.tz.tzlocal())) for i, text in zip(range(4), ['coll', 'great', 'rt', 'love you'])]
-    return render_template("main/message.html", post = message, user = user, n_replies = len(replies), replies =replies)
+    replies = [model.Message(
+            user = users[i+2],
+            text = text, 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
+        ) for i, text in zip(range(4), ['coll', 'great', 'rt', 'love you'])]
+    return render_template("main/message.html",
+        post = message,
+        user = user,
+        n_replies = len(replies), 
+        replies =replies)
