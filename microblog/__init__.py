624237cb599469ae1a48a73575db998e7e694d7d
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2021-10-30 23:31:40 +0200

    added some extra stuff

--- microblog/__init__.py
+++ microblog/__init__.py
@@ -11,9 +11,15 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SECRET_KEY"] = b"\x8c\xa5\x04\xb3\x8f\xa1<\xef\x9bY\xca/*\xff\x12\xfb"
 
-    #SQL ALCHEMY     
-    app.config["SQLALCHEMY_DATABASE_URI"] =\
-        "mysql+mysqldb://22_appweb_20:ch2663s7@mysql.lab.it.uc3m.es/22_appweb_20a"
+    #SQL ALCHEMY
+    
+    #lab
+    """
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://22_appweb_20:ch2663s7@mysql.lab.it.uc3m.es/22_appweb_20a"
+    """
+    app.config['SQLALCHEMY_DATABASE_URI']=\
+        "mysql://chris:yolo@localhost/web"
+
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 'False'
     db.init_app(app)
 
