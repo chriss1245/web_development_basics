b372f4e7bfa5ee678f4d745c353de62474f74a7c
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2021-10-31 15:01:24 +0100

    partially finished lab5

--- microblog/main.py
+++ microblog/main.py
@@ -1,75 +1,37 @@
 import datetime
 import dateutil.tz
-from . import model, default
+from . import model, default, db
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect,\
+    abort
 import flask_login
 
 bp = Blueprint("main", __name__)
 
-
+#---------------------------Home-------------------------------------
 @bp.route("/")
 @flask_login.login_required
 def index():
-    user = model.User(email= "mary@example.com", name= "mary")
-    posts = [
-        model.Message(
-            user = user,
-            text = "Test post", 
-            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            user = user,
-            text = "Why i think we live in a parties dictatorship: \
-                We do not select governors, we select parties who decide \
-                who the govenors are.", 
-            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            user = user,
-            text = "What do you guys think", 
-            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
-        )
-    ]
-    return render_template("main/index.html", posts=posts)
+    posts = model.Message.query.order_by(model.Message.timestamp.desc()).limit(10).all()
+    return render_template("main/index.html", current_user=flask_login.current_user, posts=posts)
 
 
-@bp.route("/profile")
+@bp.route("/profile/<int:user_id>")
 @flask_login.login_required
-def profile():
-    user = flask_login.current_user
+def profile(user_id):
+    user = model.User.query.filter_by(id=user_id).first_or_404()
     user.photo =  default.PHOTO
-    posts = model.Message.query.filter_by(user=user)
-    """
-    posts = [
-        model.Message(
-            user = user,
-            text = "Test post", 
-            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            user = user,
-            text = "Why i think we live in a parties dictatorship: \
-                We do not select governors, we select parties who decide \
-                who the govenors are.", 
-            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            user = user,
-            text = "What do you guys think", 
-            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
-        )
-    ]  """
+    posts = model.Message.query.filter_by(user=user).order_by(model.Message.timestamp.desc()).all()
     return render_template("main/profile.html", user=user, posts=posts)
 
-@bp.route("/messages")
+@bp.route("/message/<int:message_id>")
 @flask_login.login_required
-def messages():
-    user = model.User(email= "mary@example.com", name= "mary")
+def message(message_id):
+    message = model.Message.query.filter_by(id=message_id).first_or_404()
     users = [model.User(email= name+ "@example.com", name= name)\
          for name in ['emilio', 'pablo', 'rosa', 'hilda']]
 
-
+    """ 
     message = model.Message(
             user = user,
             text = "Why i think we live in a parties dictatorship: \
@@ -77,14 +39,43 @@ def messages():
                 who the govenors are.", 
             timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         )
-
+    """
     replies = [model.Message(
-            user = users[i+2],
+            user = users[i],
             text = text, 
             timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ) for i, text in zip(range(4), ['coll', 'great', 'rt', 'love you'])]
     return render_template("main/message.html",
         post = message,
-        user = user,
         n_replies = len(replies), 
         replies =replies)
+
+#---------------------- New Post------------------------------
+@bp.route('/new')
+@flask_login.login_required
+def new():
+    return render_template("main/new.html")
+
+@bp.route('/new', methods=['POST'])
+@flask_login.login_required
+def main_new_post():
+    content = request.form.get('new_post')
+
+    post = model.Message(
+        user=flask_login.current_user,
+        text=content,
+        timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
+    )
+    db.session.add(post)
+    db.session.commit()
+    return redirect(url_for('main.post', message_id=post.id))
+
+@bp.route('/post/<int:message_id>')
+@flask_login.login_required
+def post(message_id):
+    post = model.Message.query.filter_by(id=message_id).first_or_404()
+    """
+    NOT NEEDED BECAUSE OF first_or_404
+    if not message:
+        abort(404, 'Post id {} does not exist'.format(message_id))"""
+    return render_template("main/post.html", post=post)
